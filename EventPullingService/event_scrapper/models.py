0f0c8bdb07d3b4776e1fdb40d8b63e6603c90d77
satishpandey <satishpandey.soft@gmail.com>
2018-08-16 02:25:44 +0530

    Migrations and model updated

--- EventPullingService/event_scrapper/models.py
+++ EventPullingService/event_scrapper/models.py
@@ -8,34 +8,37 @@ from django.contrib.postgres.fields import JSONField
 
 class Venue(TimeStampedModel):
 
-    name = models.CharField(max_length=255, null=False, blank=False)
-    url_config = JSONField(null=False)
-    scraping_config = JSONField(null=False)
-    is_active = models.BooleanField(default=True)
+	name = models.CharField(max_length=255, null=False, blank=False)
+	url_config = JSONField(null=False)
+	scraping_config = JSONField(null=False)
+	is_active = models.BooleanField(default=True)
+	venue_metadata = JSONField(null=True)
 
-    def __str__(self):
-        return str(self.name)
+	def __str__(self):
+		return str(self.name)
 
 
 class Event(TimeStampedModel):
 
-    name = models.CharField(max_length=255, null=False, blank=False)
-    description = models.CharField(max_length=500, null=True, blank=True)
-    start_date = models.DateField(null=False)
-    event_start_time = models.DateTimeField(null=True)
-    end_date = models.DateField(null=False)
-    event_end_time = models.DateTimeField(null=True)
-    tags = JSONField(null=True)
-    is_all_day_event = models.BooleanField(null=False, default=False)
-    venue = models.ForeignKey(Venue, null=False)
-    category = models.CharField(max_length=255, null=True, blank=True)
-    event_url = models.CharField(max_length=255, null=True, blank=True)
-    organizer_name = models.CharField(max_length=255, null=True, blank=True)
-    organizer_url = models.CharField(max_length=255, null=True, blank=True)
-    minimum_cost = models.FloatField(default=0.0)
+	name = models.CharField(max_length=255, null=False, blank=False)
+	event_id = models.CharField(max_length=255, null=True, blank=True)
+	description = models.CharField(max_length=500, null=True, blank=True)
+	start_date = models.DateField(null=False)
+	event_start_time = models.DateTimeField(null=True)
+	end_date = models.DateField(null=False)
+	event_end_time = models.DateTimeField(null=True)
+	tags = JSONField(null=True)
+	is_all_day_event = models.BooleanField(null=False, default=False)
+	venue = models.ForeignKey(Venue, null=False)
+	category = models.CharField(max_length=255, null=True, blank=True)
+	event_url = models.CharField(max_length=255, null=True, blank=True)
+	organizer_name = models.CharField(max_length=255, null=True, blank=True)
+	organizer_url = models.CharField(max_length=255, null=True, blank=True)
+	minimum_cost = models.FloatField(default=0.0)
+	event_metadata = JSONField(null=True)
 
-    def __str__(self):
-        return str(self.venue.name) + '-' + str(self.name) + '-' + str(self.date)
+	def __str__(self):
+		return str(self.venue.name) + '-' + str(self.name) + '-' + str(self.date)
 
 
 class Config(TimeStampedModel):
@@ -47,12 +50,13 @@ class Config(TimeStampedModel):
 class ScrapingEventLogs(TimeStampedModel):
 
 	status_choices = Choices(
-        (0, 'In Progress'),
-        (1, 'Success'),
-        (2, 'Failed'),
-    )
-
+		(0, 'In Progress'),
+		(1, 'Success'),
+		(2, 'Failed'),
+	)
+	
 	status = models.IntegerField(choices=status_choices, null=False)
+	venue = models.ForeignKey(Venue, null=False)
 	start_time = models.DateTimeField(null=True)
 	end_time = models.DateTimeField(null=True)
 	description = JSONField(null=True)
